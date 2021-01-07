f5e3618737e06dd488de0fdde719a03713c7ad55
cyberboj <76927982+cyberboj@users.noreply.github.com>
2021-01-07 01:24:37 -0500

    Update fizz_buzz.java

--- my-exercises/fizz_buzz.java
+++ my-exercises/fizz_buzz.java
@@ -1,5 +1,4 @@
-
-/* 
+/*
 Write a program that outputs the string representation of numbers from 1 to n.
 But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”.
 For numbers which are multiples of both three and five output “FizzBuzz”. */
@@ -12,17 +11,20 @@ public class Practice {
         int n = 15;
 
         for (int i = 0; i <= n; i++) {
-           // System.out.println(i);
-            if( i%3 == 0) {
+            // System.out.println(i);
+            if( i%3 == 0 && i != 0) {
 
                 System.out.println(i + " is Fizz");
             }
 
-            else if (i%5 == 0) {
+            else if ( i%5 == 0 && i != 0) {
                 System.out.println(i + " is Buzz");
             }
 
-            else {
+            else if ( i%3 == 0 && i%5 == 0 ) {
+                System.out.println(i + " is FizzBuzz"); //works for 0 but not 15 and couldn't get any help to make it work so i left it like that :(
+            }
+                else {
                 System.out.println(i);
             }
 
